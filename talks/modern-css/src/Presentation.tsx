aa1f4090d79d2fff8c14b5b9520a1dd8c774f6b9
Uriell <me+github@uriell.dev>
2022-04-16 10:36:30 -0300

    initializing talks monorepo with CRA example

---
+++ talks/modern-css/src/Presentation.tsx
@@ -0,0 +1,5 @@
+function Presentation() {
+  return <h1>Hello World</h1>;
+}
+
+export default Presentation;
---
+++ talks/modern-css/src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+import Presentation from "./Presentation";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <Presentation />
+);
