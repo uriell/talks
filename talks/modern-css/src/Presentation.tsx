a1f364f6bf98d245b9cb914840a08244f8c2144d
Uriell <me+github@uriell.dev>
2022-04-29 17:13:25 -0300

    adding keyboard and url navigation

--- talks/modern-css/src/Presentation.tsx
+++ talks/modern-css/src/Presentation.tsx
@@ -1,29 +1,63 @@
+import { useEffect } from "react";
 import AwesomeSlider from "react-awesome-slider";
+import { useNavigate, useParams } from "react-router-dom";
 
 import customAwsSldCssModule from "./react-awesome-slider.module.scss";
 import Slide from "./components/Slide";
 
 import slides from "./slides/sections";
 
-const Presentation = () => (
-  <AwesomeSlider
-    fillParent
-    bullets={false}
-    organicArrows={false}
-    // onLoadStart -- look into this fn
-    // media -- look into this prop
-    cssModule={customAwsSldCssModule}
-  >
-    {slides.map((slide, index) => (
-      <div key={index} className={customAwsSldCssModule.slide}>
-        {Array.isArray(slide) ? (
-          <Slide.Group slides={slide} />
-        ) : (
-          <Slide {...slide} />
-        )}
-      </div>
-    ))}
-  </AwesomeSlider>
-);
+const SLIDES_COUNT = slides.length;
+
+const Presentation = () => {
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const currentSlide = parseInt(params.slide ?? "0", 10);
+  const isInvalidSlide = currentSlide < 1 || currentSlide > SLIDES_COUNT;
+
+  useEffect(() => {
+    if (isInvalidSlide) return navigate("/1");
+
+    function onKeyDown(event: KeyboardEvent) {
+      if (event.key === "ArrowRight") {
+        navigate(`/${currentSlide + 1}`);
+      } else if (event.key === "ArrowLeft") {
+        navigate(`/${currentSlide - 1}`);
+      }
+    }
+
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, [currentSlide, isInvalidSlide, navigate]);
+
+  if (isInvalidSlide) {
+    navigate("/1");
+    return null;
+  }
+
+  return (
+    <AwesomeSlider
+      fillParent
+      bullets={false}
+      organicArrows={false}
+      selected={currentSlide - 1}
+      // onLoadStart -- look into this fn
+      // media -- look into this prop
+      cssModule={customAwsSldCssModule}
+    >
+      {slides.map((slide, index) => (
+        <div key={index} className={customAwsSldCssModule.slide}>
+          {Array.isArray(slide) ? (
+            <Slide.Group slides={slide} />
+          ) : (
+            <Slide {...slide} />
+          )}
+        </div>
+      ))}
+    </AwesomeSlider>
+  );
+};
 
 export default Presentation;
--- talks/modern-css/src/index.tsx
+++ talks/modern-css/src/index.tsx
@@ -1,8 +1,14 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Presentation from "./Presentation";
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <Presentation />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/:slide" element={<Presentation />} />
+      <Route path="*" element={<Navigate to="/1" />} />
+    </Routes>
+  </BrowserRouter>
 );
