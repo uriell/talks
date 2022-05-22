7f61aceb7cdb74f3aa8d472db4c132270021f466
Uriell <me+github@uriell.dev>
2022-05-21 21:59:18 -0300

    refactoring CodeSurfer to use new SubSlideRouter

--- talks/modern-css/src/components/CodeSurfer.tsx
+++ talks/modern-css/src/components/CodeSurfer.tsx
@@ -1,16 +1,43 @@
 import styled from "styled-components";
 import { CodeSurfer as BaseCodeSurfer } from "@code-surfer/standalone";
 import { dracula as draculaTheme } from "@code-surfer/themes";
-import {
-  Navigate,
-  Route,
-  Routes,
-  useMatch,
-  useNavigate,
-  useParams,
-} from "react-router-dom";
-import { useEffect } from "react";
 import { useSpring } from "use-spring";
+
+import SubSlideRouter from "./SubSlideRouter";
+
+type Step = {
+  code: string;
+  focus?: string;
+  lang?: string;
+  title?: string;
+  subtitle?: string;
+  showNumbers?: boolean;
+};
+
+type InnerCodeSurferProps = {
+  steps: Step[];
+  currentSubSlide: number;
+};
+
+const InnerCodeSurfer: React.FC<InnerCodeSurferProps> = ({
+  steps,
+  currentSubSlide,
+}) => {
+  const [easedProgress] = useSpring(currentSubSlide - 1, {
+    decimals: 3,
+    stiffness: 75,
+    damping: 30,
+    mass: 4,
+  });
+
+  return (
+    <BaseCodeSurfer
+      progress={easedProgress}
+      steps={steps}
+      theme={draculaTheme}
+    />
+  );
+};
 
 type CodeSurferWrapperProps = {
   fontSize?: string;
@@ -30,90 +57,37 @@ const CodeSurferStyleWrapper = styled.div<CodeSurferWrapperProps>`
   }
 `;
 
-type Step = {
-  code: string;
-  focus?: string;
-  lang?: string;
-  title?: string;
-  subtitle?: string;
-  showNumbers?: boolean;
+type CodeSurferProps = Omit<InnerCodeSurferProps, "currentSubSlide"> & {
+  index: number;
+  backgroundColor?: string;
+  fontSize?: string;
 };
 
-type CodeSurferProps = {
-  index: number;
-  steps: Step[];
-  backgroundColor?: string;
-} & CodeSurferWrapperProps;
-
-const InnerCodeSurfer: React.FC<CodeSurferProps> = ({
+const CodeSurfer: React.FC<CodeSurferProps> = ({
   index,
-  steps,
   backgroundColor,
+  fontSize,
   ...props
 }) => {
-  const params = useParams();
-  const navigate = useNavigate();
-  const currentSubSlide = parseInt(params.subSlide ?? "0", 10);
-
-  (draculaTheme as any).colors.background = backgroundColor || "#223154";
-  (draculaTheme as any).styles.CodeSurfer.code.background =
-    backgroundColor || "#223154";
-  (draculaTheme as any).styles.CodeSurfer.pre.background =
-    backgroundColor || "#223154";
-  (draculaTheme as any).styles.CodeSurfer.title.background =
-    backgroundColor || "#223154";
-
-  useEffect(() => {
-    const isFirstStep = currentSubSlide === 1;
-    const isLastStep = currentSubSlide === steps.length;
-
-    function onKeyDown(event: KeyboardEvent) {
-      if (event.key === "ArrowRight" && !isLastStep) {
-        event.stopImmediatePropagation();
-        navigate(`/${index + 1}/step/${currentSubSlide + 1}`);
-      } else if (event.key === "ArrowLeft" && !isFirstStep) {
-        event.stopImmediatePropagation();
-        navigate(`/${index + 1}/step/${currentSubSlide - 1}`);
-      }
-    }
-
-    document.addEventListener("keydown", onKeyDown);
-
-    return () => document.removeEventListener("keydown", onKeyDown);
-  }, [currentSubSlide, index, steps.length, navigate]);
-
-  const [easedProgress] = useSpring(currentSubSlide - 1, {
-    decimals: 3,
-    stiffness: 75,
-    damping: 30,
-    mass: 4,
-  });
+  overrideTheme(draculaTheme as any, backgroundColor);
 
   return (
-    <CodeSurferStyleWrapper {...props}>
-      <BaseCodeSurfer
-        progress={easedProgress}
-        steps={steps}
-        theme={draculaTheme}
-      />
-    </CodeSurferStyleWrapper>
+    <SubSlideRouter index={index} stepsCount={props.steps.length}>
+      {(currentSubSlide) => (
+        <CodeSurferStyleWrapper fontSize={fontSize}>
+          <InnerCodeSurfer currentSubSlide={currentSubSlide} {...props} />
+        </CodeSurferStyleWrapper>
+      )}
+    </SubSlideRouter>
   );
 };
 
-const CodeSurfer: React.FC<CodeSurferProps> = ({ index, ...props }) => {
-  const match = useMatch(`/${index + 1}/*`);
-
-  if (!match) return null;
-
-  return (
-    <Routes>
-      <Route
-        path="/step/:subSlide"
-        element={<InnerCodeSurfer index={index} {...props} />}
-      />
-      <Route path="/*" element={<Navigate to={`/${index + 1}/step/1`} />} />
-    </Routes>
-  );
-};
+function overrideTheme(theme: any, backgroundColor: string = "#223154") {
+  theme.colors.background =
+    theme.styles.CodeSurfer.code.background =
+    theme.styles.CodeSurfer.pre.background =
+    theme.styles.CodeSurfer.title.background =
+      backgroundColor;
+}
 
 export default CodeSurfer;
---
+++ talks/modern-css/src/components/SubSlideRouter.tsx
@@ -0,0 +1,64 @@
+import {
+  Navigate,
+  Route,
+  Routes,
+  useMatch,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { useEffect } from "react";
+
+type InnerSubSlideProps = {
+  index: number;
+  stepsCount: number;
+  children: (currentSubSlide: number) => React.ReactElement<any, any> | null;
+};
+
+const InnerSubSlide: React.FC<InnerSubSlideProps> = ({
+  index,
+  stepsCount,
+  children,
+}) => {
+  const params = useParams();
+  const navigate = useNavigate();
+  const currentSubSlide = parseInt(params.subSlide ?? "0", 10);
+
+  useEffect(() => {
+    const isFirstStep = currentSubSlide === 1;
+    const isLastStep = currentSubSlide === stepsCount;
+
+    function onKeyDown(event: KeyboardEvent) {
+      if (event.key === "ArrowRight" && !isLastStep) {
+        event.stopImmediatePropagation();
+        navigate(`/${index + 1}/step/${currentSubSlide + 1}`);
+      } else if (event.key === "ArrowLeft" && !isFirstStep) {
+        event.stopImmediatePropagation();
+        navigate(`/${index + 1}/step/${currentSubSlide - 1}`);
+      }
+    }
+
+    document.addEventListener("keydown", onKeyDown);
+
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, [currentSubSlide, index, stepsCount, navigate]);
+
+  return children(currentSubSlide);
+};
+
+const SubSlideRouter: React.FC<InnerSubSlideProps> = (props) => {
+  const match = useMatch(`/${props.index + 1}/*`);
+
+  if (!match) return null;
+
+  return (
+    <Routes>
+      <Route path="/step/:subSlide" element={<InnerSubSlide {...props} />} />
+      <Route
+        path="/*"
+        element={<Navigate to={`/${props.index + 1}/step/1`} />}
+      />
+    </Routes>
+  );
+};
+
+export default SubSlideRouter;
